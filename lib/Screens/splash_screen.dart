1a3f6466613878381649359aaadac7cdbc36aec2
Hussnain <hussnain.waheed864@techservicesstudio.com>
2025-01-10 00:05:17 +0500

    Minor Update

--- lib/Screens/splash_screen.dart
+++ lib/Screens/splash_screen.dart
@@ -17,7 +17,7 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
 
     Timer(
-      const Duration(seconds: 5), // Correct usage
+      const Duration(seconds: 10), // Correct usage
           () {
         Navigator.pushReplacement(
           context,
